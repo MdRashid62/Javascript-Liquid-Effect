2b2d0a550b6db9e8bc5945513d1d07ae086d51b7
Muhammad Rashid <55427374+MdRashid62@users.noreply.github.com>
2020-04-22 14:15:05 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.8,
+  image1: "/img/pic1.jpg",
+  image2: "/img/pic2.jpg",
+  angle: Math.PI / 4,
+
+  displacementImage: "/img/diss.png",
+});
